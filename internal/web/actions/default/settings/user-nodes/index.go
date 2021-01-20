e0b6fed75d7f678c15ea602dbf3ebd81e9f54853
goedgecloud <goedgecloud@gmail.com>
2021-01-20 10:08:50 +0800

    显示用户节点运行状态

--- internal/web/actions/default/settings/user-nodes/index.go
+++ internal/web/actions/default/settings/user-nodes/index.go
@@ -1,9 +1,14 @@
 package usernodes
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/TeaOSLab/EdgeAdmin/internal/web/actions/actionutils"
+	"github.com/TeaOSLab/EdgeCommon/pkg/nodeconfigs"
 	"github.com/TeaOSLab/EdgeCommon/pkg/rpc/pb"
+	"github.com/iwind/TeaGo/logs"
 	"github.com/iwind/TeaGo/maps"
+	"time"
 )
 
 type IndexAction struct {
@@ -36,11 +41,32 @@ func (this *IndexAction) RunGet(params struct{}) {
 		}
 
 		for _, node := range nodesResp.Nodes {
+			// 状态
+			status := &nodeconfigs.NodeStatus{}
+			if len(node.StatusJSON) > 0 {
+				err = json.Unmarshal(node.StatusJSON, &status)
+				if err != nil {
+					logs.Error(err)
+					continue
+				}
+				status.IsActive = status.IsActive && time.Now().Unix()-status.UpdatedAt <= 60 // N秒之内认为活跃
+			}
+
 			nodeMaps = append(nodeMaps, maps.Map{
 				"id":          node.Id,
 				"isOn":        node.IsOn,
 				"name":        node.Name,
 				"accessAddrs": node.AccessAddrs,
+				"status": maps.Map{
+					"isActive":     status.IsActive,
+					"updatedAt":    status.UpdatedAt,
+					"hostname":     status.Hostname,
+					"cpuUsage":     status.CPUUsage,
+					"cpuUsageText": fmt.Sprintf("%.2f%%", status.CPUUsage*100),
+					"memUsage":     status.MemoryUsage,
+					"memUsageText": fmt.Sprintf("%.2f%%", status.MemoryUsage*100),
+					"buildVersion": status.BuildVersion,
+				},
 			})
 		}
 	}
