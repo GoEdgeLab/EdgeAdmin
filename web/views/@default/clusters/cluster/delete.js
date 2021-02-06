48fd527fff3940ef0bd90765486210260fb0d5da
goedgecloud <goedgecloud@gmail.com>
2021-02-06 19:40:29 +0800

    优化交互

--- web/views/@default/clusters/cluster/delete.js
+++ web/views/@default/clusters/cluster/delete.js
@@ -1,14 +1,16 @@
 Tea.context(function () {
-	this.deleteCluster = function (clusterId) {
-		let that = this
-		teaweb.confirm("确定要删除此集群吗？", function () {
-			that.$post("/clusters/cluster/delete")
-				.params({
-					clusterId: clusterId
-				})
-				.success(function () {
-					window.location = "/clusters"
-				})
-		})
-	}
+    this.deleteCluster = function (clusterId) {
+        let that = this
+        teaweb.confirm("确定要删除此集群吗？", function () {
+            that.$post("/clusters/cluster/delete")
+                .params({
+                    clusterId: clusterId
+                })
+                .success(function () {
+                    teaweb.success("删除成功", function () {
+                        window.location = "/clusters"
+                    })
+                })
+        })
+    }
 })
