2bf58e5c35f7d13445a93ec935f6dd56f2090a70
goedgecloud <goedgecloud@gmail.com>
2021-06-07 16:23:37 +0800

    优化界面

---
+++ web/views/@default/servers/addOriginPopup.js
@@ -0,0 +1,44 @@
+Tea.context(function () {
+	this.addr = ""
+	this.protocol = ""
+
+	this.addrError = ""
+
+	if (this.serverType == "httpProxy") {
+		this.protocol = "http"
+	}
+
+	this.changeProtocol = function () {
+		this.checkPort()
+	}
+
+	this.changeAddr = function () {
+		if (this.serverType == "httpProxy") {
+			if (this.addr.startsWith("http://")) {
+				this.protocol = "http"
+			} else if (this.addr.startsWith("https://")) {
+				this.protocol = "https"
+			}
+		}
+
+		this.checkPort()
+	}
+
+	this.checkPort = function () {
+		this.addrError = ""
+
+		// HTTP
+		if (this.protocol == "http") {
+			if (this.addr.endsWith(":443")) {
+				this.addrError = "443通常是HTTPS协议端口，请确认源站协议选择是否正确。"
+			}
+		}
+
+		// HTTPS
+		if (this.protocol == "https") {
+			if (this.addr.endsWith(":80")) {
+				this.addrError = "80通常是HTTP协议端口，请确认源站协议选择是否正确。"
+			}
+		}
+	}
+})
