f5b702066f6b41e09fe6c17ecda995ffc123deb5
goedgecloud <goedgecloud@gmail.com>
2021-07-08 16:23:51 +0800

    优化关键词显示

--- web/public/js/components/common/keyword.js
+++ web/public/js/components/common/keyword.js
@@ -4,12 +4,18 @@ Vue.component("keyword", {
 		let word = this.vWord
 		if (word == null) {
 			word = ""
+		} else {
+			word = word.replace(/\)/, "\\)")
+			word = word.replace(/\(/, "\\(")
+			word = word.replace(/\+/, "\\+")
+			word = word.replace(/\^/, "\\^")
+			word = word.replace(/\$/, "\\$")
 		}
 
 		let slot = this.$slots["default"][0]
 		let text = slot.text
 		if (word.length > 0) {
-			text = text.replace(new RegExp(word, "g"), "<span style=\"border: 1px #ccc dashed; color: #ef4d58\">" + word + "</span>")
+			text = text.replace(new RegExp("(" + word + ")", "ig"), "<span style=\"border: 1px #ccc dashed; color: #ef4d58\">$1</span>")
 		}
 
 		return {
