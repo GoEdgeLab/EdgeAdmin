53e82af97710399866f49c150828a5ff78dfe8fe
goedgecloud <goedgecloud@gmail.com>
2021-08-20 11:27:06 +0800

    DNS访问日志显示匹配的线路

--- web/public/js/components/ns/ns-access-log-box.js
+++ web/public/js/components/ns/ns-access-log-box.js
@@ -34,6 +34,9 @@ Vue.component("ns-access-log-box", {
 	},
 	template: `<div class="access-log-row" :style="{'color': (accessLog.nsRecordId == null || accessLog.nsRecordId == 0) ? '#dc143c' : ''}" ref="box">
 	<span v-if="accessLog.region != null && accessLog.region.length > 0" class="grey">[{{accessLog.region}}]</span> <keyword :v-word="vKeyword">{{accessLog.remoteAddr}}</keyword> [{{accessLog.timeLocal}}] [{{accessLog.networking}}] <em>{{accessLog.questionType}} <keyword :v-word="vKeyword">{{accessLog.questionName}}</keyword></em> -&gt; <em>{{accessLog.recordType}} <keyword :v-word="vKeyword">{{accessLog.recordValue}}</keyword></em><!-- &nbsp; <a href="" @click.prevent="showLog" title="查看详情"><i class="icon expand"></i></a>-->
+	<div v-if="accessLog.nsRoutes != null && accessLog.nsRoutes.length > 0" style="margin-top: 0.3em">
+		<span class="ui label tiny basic grey" v-for="route in accessLog.nsRoutes">线路: {{route.name}}</span>
+	</div>
 	<div v-if="accessLog.error != null && accessLog.error.length > 0">
 		<i class="icon warning circle"></i>错误：[{{accessLog.error}}]
 	</div>
