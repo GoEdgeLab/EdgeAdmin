0bde2a5be0038d6b6ed84dc48fc4d290fc9f0f60
goedgecloud <goedgecloud@gmail.com>
2021-10-29 12:21:52 +0800

    支持gif转webp

--- web/public/js/components/server/http-webp-config-box.js
+++ web/public/js/components/server/http-webp-config-box.js
@@ -9,7 +9,7 @@ Vue.component("http-webp-config-box", {
 				quality: 50,
 				minLength: {count: 0, "unit": "kb"},
 				maxLength: {count: 0, "unit": "kb"},
-				mimeTypes: ["image/png", "image/jpeg", "image/bmp", "image/x-ico"],
+				mimeTypes: ["image/png", "image/jpeg", "image/bmp", "image/x-ico", "image/gif"],
 				extensions: [".png", ".jpeg", ".jpg", ".bmp", ".ico"],
 				conds: null
 			}
