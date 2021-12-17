a6aa29052691a783529feaf5942f5d16c48bf0e1
goedgecloud <goedgecloud@gmail.com>
2021-12-17 14:25:40 +0800

    调整单次通知更新任务的数量为500（原来200）

--- internal/tasks/task_sync_cluster.go
+++ internal/tasks/task_sync_cluster.go
@@ -51,7 +51,7 @@ func (this *SyncClusterTask) loop() error {
 	}
 	ctx := rpcClient.Context(0)
 
-	tasksResp, err := rpcClient.NodeTaskRPC().FindNotifyingNodeTasks(ctx, &pb.FindNotifyingNodeTasksRequest{Size: 200})
+	tasksResp, err := rpcClient.NodeTaskRPC().FindNotifyingNodeTasks(ctx, &pb.FindNotifyingNodeTasksRequest{Size: 500})
 	if err != nil {
 		return err
 	}
