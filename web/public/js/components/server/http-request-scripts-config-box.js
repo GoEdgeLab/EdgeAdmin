eab86c47f76a09b72b50708a06e2edcecb3397ee
goedgecloud <goedgecloud@gmail.com>
2021-12-31 19:45:14 +0800

    优化文字

--- web/public/js/components/server/http-request-scripts-config-box.js
+++ web/public/js/components/server/http-request-scripts-config-box.js
@@ -22,11 +22,13 @@ Vue.component("http-request-scripts-config-box", {
 	template: `<div>
 	<input type="hidden" name="requestScriptsJSON" :value="JSON.stringify(config)"/>
 	<div class="margin"></div>
-	<h4>请求初始化</h4>
+	<h4 style="margin-bottom: 0">请求初始化</h4>
+	<p class="comment">在请求刚初始化时调用，此时自定义Header等尚未生效。</p>
 	<div>
 		<script-config-box id="init-script" :v-script-config="config.onInitScript" comment="在接收到客户端请求之后立即调用。预置req、resp变量。" @change="changeInitScript"></script-config-box>
 	</div>
-	<h4>准备发送请求</h4>
+	<h4 style="margin-bottom: 0">准备发送请求</h4>
+	<p class="comment">在准备执行请求或者转发请求之前调用，此时自定义Header、源站等已准备好。</p>
 	<div>
 		<script-config-box id="request-script" :v-script-config="config.onRequestScript" comment="在准备好转发客户端请求之前调用。预置req、resp变量。" @change="changeRequestScript"></script-config-box>
 	</div>
