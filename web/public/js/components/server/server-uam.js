41185c4b2fc2e1ecb1f491c8389abfc3a1bff1ed
goedgecloud <goedgecloud@gmail.com>
2022-04-04 19:47:57 +0800

    优化界面

--- web/public/js/components/server/server-uam.js
+++ web/public/js/components/server/server-uam.js
@@ -19,7 +19,7 @@ Vue.component("uam-config-box", {
 		<td class="title">启用5秒盾</td>
 		<td>
 			<checkbox v-model="config.isOn"></checkbox>
-			<p class="comment">启用后，访问网站时，自动检查浏览器环境，阻止非正常访问。</p>
+			<p class="comment"><plus-label></plus-label>启用后，访问网站时，自动检查浏览器环境，阻止非正常访问。</p>
 		</td>
 	</tr>
 </table>
