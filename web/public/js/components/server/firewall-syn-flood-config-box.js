38be47aa77b0952c8186d78cbba62f6cba6d2bf7
goedgecloud <goedgecloud@gmail.com>
2022-11-01 21:16:48 +0800

    修改一处注释

--- web/public/js/components/server/firewall-syn-flood-config-box.js
+++ web/public/js/components/server/firewall-syn-flood-config-box.js
@@ -59,7 +59,7 @@ Vue.component("firewall-syn-flood-config-box", {
 			<td class="title">启用</td>
 			<td>
 				<checkbox v-model="config.isOn"></checkbox>
-				<p class="comment">启用后，WAF将会尝试自动检测并阻止SYN Flood攻击。此功能需要节点已安装并启用Firewalld。</p>
+				<p class="comment">启用后，WAF将会尝试自动检测并阻止SYN Flood攻击。此功能需要节点已安装并启用nftables或Firewalld。</p>
 			</td>
 		</tr>
 		<tr>
