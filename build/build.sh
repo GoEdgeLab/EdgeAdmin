dd9025ee85edc34464d033a2dac3cdb9b0c3d15d
goedgecloud <goedgecloud@gmail.com>
2023-03-17 10:20:11 +0800

    编译脚本中暂时不删除.css.map和.js.map文件

--- build/build.sh
+++ build/build.sh
@@ -105,8 +105,8 @@ function build() {
 	find "$DIST" -name ".DS_Store" -delete
 	find "$DIST" -name ".gitignore" -delete
 	find "$DIST" -name "*.less" -delete
-	find "$DIST" -name "*.css.map" -delete
-	find "$DIST" -name "*.js.map" -delete
+	#find "$DIST" -name "*.css.map" -delete
+	#find "$DIST" -name "*.js.map" -delete
 
 	# zip
 	echo "zip files ..."
