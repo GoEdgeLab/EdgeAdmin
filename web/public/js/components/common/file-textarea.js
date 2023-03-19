f3f0482d6ccd5660bdaff1b05083c669ee85b360
goedgecloud <goedgecloud@gmail.com>
2023-03-19 17:49:07 +0800

    SSH认证添加私钥时可以从私钥文件中直接拖入

---
+++ web/public/js/components/common/file-textarea.js
@@ -0,0 +1,26 @@
+Vue.component("file-textarea", {
+	props: ["value"],
+	data: function () {
+		let value = this.value
+		if (typeof value != "string") {
+			value = ""
+		}
+		return {
+			realValue: value
+		}
+	},
+	mounted: function () {
+	},
+	methods: {
+		drop: function (e) {
+			let that = this
+			e.dataTransfer.items[0].getAsFile().text().then(function (data) {
+				that.setValue(data)
+			})
+		},
+		setValue: function (value) {
+			this.realValue = value
+		}
+	},
+	template: `<textarea @drop.prevent="drop" ref="textarea" v-model="realValue"></textarea>`
+})
