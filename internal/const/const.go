93721e4b47ed467875da3c24e7b81671692ca807
goedgecloud <goedgecloud@gmail.com>
2023-05-28 16:06:05 +0800

    版本号改为1.1.0

--- internal/const/const.go
+++ internal/const/const.go
@@ -1,9 +1,9 @@
 package teaconst
 
 const (
-	Version = "1.0.4"
+	Version = "1.1.0"
 
-	APINodeVersion = "1.0.4"
+	APINodeVersion = "1.1.0"
 
 	ProductName   = "Edge Admin"
 	ProcessName   = "edge-admin"
