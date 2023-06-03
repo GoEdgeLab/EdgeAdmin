1e1c97e36d54b6ae99015fd0fb718f0f23459f8b
goedgecloud <goedgecloud@gmail.com>
2023-06-03 09:09:18 +0800

    更新components.js

--- web/public/js/components.src.js
+++ web/public/js/components.src.js
@@ -3894,7 +3894,8 @@ Vue.component("ssl-config-box", {
 	props: [
 		"v-ssl-policy",
 		"v-protocol",
-		"v-server-id"
+		"v-server-id",
+		"v-support-http3"
 	],
 	created: function () {
 		let that = this
@@ -3918,6 +3919,7 @@ Vue.component("ssl-config-box", {
 				cipherSuitesIsOn: false,
 				cipherSuites: [],
 				http2Enabled: true,
+				http3Enabled: false,
 				ocspIsOn: false
 			}
 		} else {
@@ -4291,6 +4293,15 @@ Vue.component("ssl-config-box", {
 				<td>
 					<div class="ui checkbox">
 						<input type="checkbox" value="1" v-model="policy.http2Enabled"/>
+						<label></label>
+					</div>
+				</td>
+			</tr>
+			<tr v-show="vProtocol == 'https' && vSupportHttp3">
+				<td class="title">启用HTTP/3</td>
+				<td>
+					<div class="ui checkbox">
+						<input type="checkbox" value="1" v-model="policy.http3Enabled"/>
 						<label></label>
 					</div>
 				</td>
