7b6655338100c2e0fcc73074a5ac005bd3a13101
goedgecloud <goedgecloud@gmail.com>
2023-07-28 09:25:04 +0800

    修复分组设置无法打开的问题

--- internal/web/actions/default/servers/groups/group/settings/index/init.go
+++ internal/web/actions/default/servers/groups/group/settings/index/init.go
@@ -2,7 +2,6 @@ package reverseProxy
 
 import (
 	"github.com/TeaOSLab/EdgeAdmin/internal/configloaders"
-	"github.com/TeaOSLab/EdgeAdmin/internal/web/actions/default/servers/server/settings/locations/locationutils"
 	"github.com/TeaOSLab/EdgeAdmin/internal/web/actions/default/servers/serverutils"
 	"github.com/TeaOSLab/EdgeAdmin/internal/web/helpers"
 	"github.com/iwind/TeaGo"
@@ -12,7 +11,6 @@ func init() {
 	TeaGo.BeforeStart(func(server *TeaGo.Server) {
 		server.
 			Helper(helpers.NewUserMustAuth(configloaders.AdminModuleCodeServer)).
-			Helper(locationutils.NewLocationHelper()).
 			Helper(serverutils.NewServerHelper()).
 			Data("mainTab", "setting").
 			Prefix("/servers/groups/group/settings").
