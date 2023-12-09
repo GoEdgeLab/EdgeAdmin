8c995ed4a752277d4497aa3a6e689254ece0cbda
goedgecloud <goedgecloud@gmail.com>
2023-12-09 15:55:17 +0800

    优化WAF动作“显示网页”显示

--- web/public/js/components/server/http-firewall-actions-view.js
+++ web/public/js/components/server/http-firewall-actions-view.js
@@ -3,7 +3,11 @@ Vue.component("http-firewall-actions-view", {
 	props: ["v-actions"],
 	template: `<div>
 		<div v-for="action in vActions" style="margin-bottom: 0.3em">
-			<span :class="{red: action.category == 'block', orange: action.category == 'verify', green: action.category == 'allow'}">{{action.name}} ({{action.code.toUpperCase()}})</span>
+			<span :class="{red: action.category == 'block', orange: action.category == 'verify', green: action.category == 'allow'}">{{action.name}} ({{action.code.toUpperCase()}})
+			  	<div v-if="action.options != null">
+			  		<span class="grey small" v-if="action.code.toLowerCase() == 'page'">[{{action.options.status}}]</span>
+				</div>	
+			</span>
 		</div>             
 </div>`
 })
