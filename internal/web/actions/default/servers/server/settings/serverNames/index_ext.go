f9c1901e8a30c4bdefa1e4739fe2bde73c413cab
goedgecloud <goedgecloud@gmail.com>
2023-12-18 10:12:20 +0800

    修复一处编译问题

--- internal/web/actions/default/servers/server/settings/serverNames/index_ext.go
+++ internal/web/actions/default/servers/server/settings/serverNames/index_ext.go
@@ -3,6 +3,8 @@
 
 package serverNames
 
+import "github.com/TeaOSLab/EdgeCommon/pkg/serverconfigs"
+
 func (this *IndexAction) checkPlan(serverId int64, serverNames []*serverconfigs.ServerNameConfig) {
 	// stub
 }
