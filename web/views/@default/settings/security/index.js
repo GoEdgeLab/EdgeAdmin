10ff67aad2913627a338979e05c1d849c14d074e
goedgecloud <goedgecloud@gmail.com>
2024-03-10 12:02:41 +0800

    优化安全设置中“自定义客户端IP报头”中默认报头交互

--- web/views/@default/settings/security/index.js
+++ web/views/@default/settings/security/index.js
@@ -1,3 +1,11 @@
 Tea.context(function () {
 	this.success = NotifyReloadSuccess("保存成功")
+
+	this.addDefaultClientIPHeaderNames = function (headerNames) {
+		if (this.config.clientIPHeaderNames == null || this.config.clientIPHeaderNames.length == 0) {
+			this.config.clientIPHeaderNames = headerNames
+		} else {
+			this.config.clientIPHeaderNames += " " + headerNames
+		}
+	}
 })
